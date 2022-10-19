a21b414c4702a49cdfa9ec4eef3d29bea23cf1fa
Danilx8 <lugovskihdanil@yandex.ru>
2022-10-19 11:52:48 +0700

    Roman to Arabic numbers translation done

--- Roman_Arabic.cpp
+++ Roman_Arabic.cpp
@@ -11,28 +11,38 @@ using namespace std;
 int main() {
     int arabicNumber, currentNumeral;
     string romanOneToTenNumbers[10] =
-        {" ", "I", "II", "III", "IV", "V", 
+        {"", "I", "II", "III", "IV", "V", 
         "VI", "VII", "VIII", "IX"},
         romanTenToNintyNumbers[10] = 
-        {" ", "X", "XX", "XXX", "XL", "L",
+        {"", "X", "XX", "XXX", "XL", "L",
         "LX", "LXX", "LXXX", "XC"},
-        romanOneToNineHunderNumbers[10] = 
-        {" ", "C", "CC", "CCC", "CD", "D",
+        romanOneToNineHundertNumbers[10] = 
+        {"", "C", "CC", "CCC", "CD", "D",
         "DC", "DCC", "DCCC", "CM"};
     
     
 	cin >> arabicNumber;
 	int iteratedNumber = arabicNumber;
     
-    string result = " ";
+    string result = "";
     
     int iteration = 1;
-    while (iteratedNumber > 0) {
+    while (iteratedNumber) {
         currentNumeral = iteratedNumber % 10;
-        iteratedNumber /= 10;
         if (iteration == 1) {
             result = romanOneToTenNumbers[currentNumeral] + result;
-        }
+        } else if (iteration == 2) {
+        	result = romanTenToNintyNumbers[currentNumeral] + result;
+		} else if (iteration == 3) {
+			result = romanOneToNineHundertNumbers[currentNumeral] + result;
+		} else {
+			while (iteratedNumber) {
+				result = "M" + result;
+				--iteratedNumber;
+			}
+			break;
+		}
+		iteratedNumber /= 10;
         iteration++;
     }
 	cout << result;
