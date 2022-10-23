368ef8201467ef0dd47fb535c4db2c1dd1e3d225
Danilx8 <lugovskihdanil@yandex.ru>
2022-10-23 21:59:38 +0700

    Added an array of shifts of the first day of a year, according to its index in a loop of 28 years, an array of weekdays, and a wrong index counter

--- Calendar.cpp
+++ Calendar.cpp
@@ -7,7 +7,20 @@
 ************************/
 
 #include <iostream>
+#include <string>
 
+int firstDayOfYearInLoop[28] = {
+	0, 1, 2, 3,    5, 6,
+	0, 1,    3, 4, 5, 6,
+	   1, 2, 3, 4,    6, 
+	0, 1, 2,    4, 5, 6, 
+	0,    2, 3, 4, 5
+	};
+
+std::string weekdays[7] = {
+	"Monday", "Tuesday", "Wednesday", "Thursday",
+	"Friday", "Saturday", "Sunday"
+	};
 
 bool isLeapYear(int year) {
 	if (year % 400 == 0) {
@@ -22,10 +35,20 @@ bool isLeapYear(int year) {
 	return false;
 }
 
+std::string firstDayOfYear(int year) {
+	int century = year / 100;
+	int index = ((4 * century) + (year % 100)) % 28;
+	int shift = (index + (index / 4)) % 7;
+	
+	return weekdays[firstDayOfYearInLoop[shift]];
+}
 
 int main() {
-	int yearNumber;
-	std::cin >> yearNumber;
+	int year;
+	std::cin >> year;
 	
-	std::cout << isLeapYear(yearNumber);
+	std::cout << isLeapYear(year) << std::endl;
+	std::cout << firstDayOfYear(year);
+	
+	return 0;
 }
