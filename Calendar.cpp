4ef26c328a88c667d5ffb58b690444756a0d2948
Danilx8 <lugovskihdanil@yandex.ru>
2022-10-24 21:46:29 +0700

    Added a fine working counter for the first weekday of each month and made a fancy output for the calendar, however with not months output yet

--- Calendar.cpp
+++ Calendar.cpp
@@ -14,6 +14,10 @@ std::string weekdays[7] = {
 	"FRI", "SAT", "SUN"
 	};
 
+int daysInMonths[12] {
+	31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+};
+
 bool isLeapYear(int year) {
 	if (year % 400 == 0) {
 		return true;
@@ -27,15 +31,21 @@ bool isLeapYear(int year) {
 	return false;
 }
 
-int firstDayOfYear(int year) {
+int firstDayOfYear(int year, int month) {	
+	int shifts[12] = {0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5};
+    int shift = shifts[month - 1];
+    
+    if (isLeapYear(year) and (month >2))
+    {
+        shift += 1;
+    };
+    
 	year = (year - 1) % 400;
-
     int century = year / 100;
-
     int index = ((4 * century) + (year % 100)) % 28;
 
-    int weekday = (index + (index / 4));
-
+    int weekday = (index + (index / 4)) + shift;
+    
     return (weekday % 7);
 }
 
@@ -43,12 +53,30 @@ int main() {
 	int year;
 	std::cin >> year;
 	
-	
-	for (int months = 0; months < 12; ++months) {
-		std::cout << "\n\n\n";
+	for (int monthIndex = 1; monthIndex < 13; ++monthIndex) {
 		for (int weekdaysIndex = 0; weekdaysIndex < 7; ++weekdaysIndex) {
-			std:: cout << weekdays[weekdaysIndex] << "   ";
+			std::cout << weekdays[weekdaysIndex] << '\t';
 		}
+		std::cout << '\n';
+		
+		int numberInWeek = 0;
+		
+		int spacesBeforeFirstDayMultiplier = firstDayOfYear(year, monthIndex);
+		while (spacesBeforeFirstDayMultiplier) {
+				std::cout << '\t';
+				--spacesBeforeFirstDayMultiplier;
+				++numberInWeek;
+			}
+			
+		for (int dayIndex = 1; dayIndex < daysInMonths[monthIndex-1] + 1; ++dayIndex) {
+			std::cout << dayIndex << '\t';
+			++numberInWeek;
+			if (numberInWeek == 7) {
+				std::cout << '\n';
+				numberInWeek = 0;
+			}
+		}
+		std::cout << "\n\n\n";
 	}
 	
 	return 0;
