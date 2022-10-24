447f8fa4423e84939f8ad782c8a023437a9b3441
Danilx8 <lugovskihdanil@yandex.ru>
2022-10-24 22:03:56 +0700

    Final commit. Made a months' names output, added the 29th day for every leap February, and brushed it up a liittle

--- Calendar.cpp
+++ Calendar.cpp
@@ -10,74 +10,84 @@
 #include <string>
 
 std::string weekdays[7] = {
-	"MON", "TUE", "WED", "THU",
-	"FRI", "SAT", "SUN"
-	};
+  "MON", "TUE", "WED", "THU",
+  "FRI", "SAT", "SUN"
+  };
+
+std:: string monthsNames[12] {
+  "January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November",
+  "December"
+  };
 
 int daysInMonths[12] {
-	31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
-};
+  31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+  };
 
 bool isLeapYear(int year) {
-	if (year % 400 == 0) {
-		return true;
-	}
-	if (year % 100 == 0) {
-		return false;
-	}
-	if (year % 4 == 0) {
-		return true;
-	}
-	return false;
-}
+  if (year % 400 == 0) {
+    return true;
+    }
+  if (year % 100 == 0) {
+    return false;
+    }
+  if (year % 4 == 0) {
+    return true;
+    }
+  return false;
+  }
 
-int firstDayOfYear(int year, int month) {	
-	int shifts[12] = {0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5};
-    int shift = shifts[month - 1];
-    
-    if (isLeapYear(year) and (month >2))
-    {
-        shift += 1;
+int firstDayOfYear(int year, int month) {
+  int shifts[12] = {0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5};
+  int shift = shifts[month - 1];
+
+  if (isLeapYear(year) and (month >2)) {
+    shift += 1;
     };
-    
-	year = (year - 1) % 400;
-    int century = year / 100;
-    int index = ((4 * century) + (year % 100)) % 28;
 
-    int weekday = (index + (index / 4)) + shift;
-    
-    return (weekday % 7);
-}
+  year = (year - 1) % 400;
+  int century = year / 100;
+  int index = ((4 * century) + (year % 100)) % 28;
+
+  int weekday = (index + (index / 4)) + shift;
+
+  return (weekday % 7);
+  }
 
 int main() {
-	int year;
-	std::cin >> year;
-	
-	for (int monthIndex = 1; monthIndex < 13; ++monthIndex) {
-		for (int weekdaysIndex = 0; weekdaysIndex < 7; ++weekdaysIndex) {
-			std::cout << weekdays[weekdaysIndex] << '\t';
-		}
-		std::cout << '\n';
-		
-		int numberInWeek = 0;
-		
-		int spacesBeforeFirstDayMultiplier = firstDayOfYear(year, monthIndex);
-		while (spacesBeforeFirstDayMultiplier) {
-				std::cout << '\t';
-				--spacesBeforeFirstDayMultiplier;
-				++numberInWeek;
-			}
-			
-		for (int dayIndex = 1; dayIndex < daysInMonths[monthIndex-1] + 1; ++dayIndex) {
-			std::cout << dayIndex << '\t';
-			++numberInWeek;
-			if (numberInWeek == 7) {
-				std::cout << '\n';
-				numberInWeek = 0;
-			}
-		}
-		std::cout << "\n\n\n";
-	}
-	
-	return 0;
-}
+  int year;
+  std::cin >> year;
+
+  for (int monthIndex = 1; monthIndex < 13; ++monthIndex) {
+    std::cout << '\n' << monthsNames[monthIndex-1] << '\n';
+    for (int weekdaysIndex = 0; weekdaysIndex < 7; ++weekdaysIndex) {
+      std::cout << weekdays[weekdaysIndex] << '\t';
+      }
+    std::cout << '\n';
+
+    int numberInWeek = 0;
+
+    int spacesBeforeFirstDayMultiplier = firstDayOfYear(year, monthIndex);
+    while (spacesBeforeFirstDayMultiplier) {
+      std::cout << '\t';
+      --spacesBeforeFirstDayMultiplier;
+      ++numberInWeek;
+      }
+
+
+    for (int dayIndex = 1; dayIndex < daysInMonths[monthIndex-1] + 1; ++dayIndex) {
+      std::cout << dayIndex << '\t';
+      ++numberInWeek;
+      if (numberInWeek == 7) {
+        std::cout << '\n';
+        numberInWeek = 0;
+        }
+      if (monthIndex == 2 && isLeapYear(year) && dayIndex == 28) {
+        std::cout << ++dayIndex;
+        }
+      }
+    std::cout << '\n';
+    }
+
+  return 0;
+  }
